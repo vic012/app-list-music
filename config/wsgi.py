90ccbd9039d99b217f8bea389ad31c87bf2fae74
Pedro Henrique <henriquevic012@gmail.com>
2021-09-28 17:44:39 -0300

    Consertando o Procfile e deploy

--- config/wsgi.py
+++ config/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
