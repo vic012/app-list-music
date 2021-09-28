4fb9a59c6bd5b8cf1b433cc2244cb43badee18f1
Pedro Henrique <henriquevic012@gmail.com>
2021-09-28 17:35:09 -0300

    Ajustando o Allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*3ydv1s@8p#r+^dm^564wohtsoskr=s7c-5v%+xg28o*2o4fm@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['starsmusic.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
